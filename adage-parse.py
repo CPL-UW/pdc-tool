0f4d3987e296b4f353bacc5781a62101c60f58d5
M Berland <matthew@berland.org>
2014-04-02 13:44:12 -0500

    added csvfy

--- adage-parse.py
+++ adage-parse.py
@@ -6,14 +6,30 @@ parser.add_argument('-f', metavar='FILE.json', type=str, help='ADAGE API formatt
 parser.add_argument('--elem', metavar='FIELD', type=str, help='Field in data (e.g., user_id)', nargs='+')
 parser.add_argument('--usersum', action="store_true", help='numeric field(s) summed over a user_id')
 parser.add_argument('--totalsum', action="store_true", help='numeric field(s) summed over the entire file')
+parser.add_argument('--csvfy', action="store_true", help='turns entire file into CSV (note: 2n for n lines to get all fields)')
 args = parser.parse_args()
-
-print args
 
 jsonfile = open(args.f,'rb')
 data = json.loads(jsonfile.read())
 
-if args.fields:
+if args.csvfy:
+    columns = defaultdict(bool)
+    for line in data:
+        for col in line:
+            columns[col] = True
+    for col in sorted(columns):
+        print col,',',
+    print ''
+    for line in data:
+        lineout = defaultdict(str)
+        outputlist = []
+        for elem in line:
+            lineout[elem] = line[elem]
+        for col in sorted(columns):
+            outputlist.append(str(lineout[col]))
+        print "\"",'","'.join(outputlist),"\""
+
+elif args.fields:
     searchfields = args.fields
 
     for elem in data:
