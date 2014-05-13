a30040eb239b1a850d42fc4ac80d2820ae546bad
dennispr <dennispr@gmail.com>
2014-05-13 14:32:22 -0500

    Added Upload and Download logic.

--- WebTool.py
+++ WebTool.py
@@ -1,6 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = set(['json'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 '''	Target date, 5/15/2014
 	TODO: Upload/Download functionality working
@@ -18,16 +24,36 @@ def analysis():
     '''choose analysis or upload a new file'''
 
 
-@app.route('/Upload')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    return 'Upload Page'
+	if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
     
-@app.route('/Output')
-def output():
-    return 'output Page'
+@app.route('/output/<filename>')
+def output(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
     '''allows the download of the correct csv file'''
     
-@app.route('/Csvfy')
+@app.route('/csvfy')
 def csvfy():
     return 'Upload Page'
     
